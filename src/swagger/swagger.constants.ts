a9ca02c46a552e32627a6bdf9ab72a8a0feb9bbb
Mike <goldbyol@outlook.com>
2021-12-11 09:07:12 -0800

    update "swagger" constants

--- src/swagger/swagger.constants.ts
+++ src/swagger/swagger.constants.ts
@@ -3,6 +3,6 @@ import { API_PREFIX } from '../shared/constants/global.constants';
 
 // Export all swagger constants
 export const SWAGGER_API_PATH = `${API_PREFIX}/api-docs`;
-export const SWAGGER_TITLE = 'MVP MATCH';
-export const SWAGGER_DESCRIPTION = 'MVP API Documentation';
+export const SWAGGER_TITLE = 'MVP Match Challenge API Documentation';
+export const SWAGGER_DESCRIPTION = 'This is API Doc for MVP Match Challenge';
 export const SWAGGER_VERSION = '1.0.0';
